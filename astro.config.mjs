79be3037530b6b50787d11e20c39aa8a7af38b51
Vardhaman Bhandari <97441447+Vardhaman619@users.noreply.github.com>
2025-03-18 03:18:29 +0000

    added Vercel adapter and updated dependencies in configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,18 @@ import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import { VitePWA } from 'vite-plugin-pwa';
-
 import icon from 'astro-icon';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://foundationrepairadelaide.com.au',
   output: "static",
   integrations: [tailwind(), sitemap(), icon()],
+
   build: {
     inlineStylesheets: 'auto'
   },
+
   vite: {
     plugins: [
       VitePWA({
@@ -66,11 +68,14 @@ export default defineConfig({
       }
     }
   },
+
   env: {
     schema: {
       SENDGRID_API_KEY: envField.string({ context: "server", access: "secret" }),
       SENDER_MAIL_ADDRESS: envField.string({ context: "server", access: "secret" }),
       RECEIVER_MAIL_ADDRESS: envField.string({ context: "server", access: "secret" })
     }
-  }
+  },
+
+  adapter: vercel()
 });
